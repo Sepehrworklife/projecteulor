d866d7d6a5be83e67d5e1cf37901af6b5bf8afcb
sajadRafati <107307586+SajadRafati@users.noreply.github.com>
2022-07-04 10:36:16 -0700

    Update 010.py

--- 010.py
+++ 010.py
@@ -8,11 +8,8 @@ Created on Sun Jul  3 00:04:13 2022
 # Summation of primes
 # Find the sum of all the primes below two million.
 def sum_prime():
-    # lower = 1
-    # upper = 200000
     x = []
-
-    for num in range(1, 10 + 1):
+    for num in range(1, 2000000 + 1):
         # all prime numbers are greater than 1
         if num > 1:
             for i in range(2, num):
